9b1d8e0b1d76e3b5266a852d1717a56026444ea2
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-24 16:16:19 +0400

    feat: get data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import viteLogo from "/vite.svg";
+import axios from "axios";
+interface Person {
+  firstName: string;
+  lastName: string;
+  zip: number;
+  email: string;
+  city: string;
+}
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [persons, setPersons] = useState<Person[]>([]);
+  useEffect(() => {
+    const getPersons = async () => {
+      try {
+        const response = await axios.get(
+          "https://web-scraping-api-4zao.onrender.com/api/person"
+        );
+        setPersons(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPersons();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>List</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
