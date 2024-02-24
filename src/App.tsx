957cc47e2ea0e29c6d1ef0a76cceb981cc4242c4
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-24 16:17:33 +0400

    style: style data

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,28 @@ function App() {
   return (
     <div>
       <h1>List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Zip Code</th>
+            <th>Email</th>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+          {persons.map((person, index) => (
+            <tr key={index} style={{ margin: "60px" }}>
+              <td>{person.firstName}</td>
+              <td>{person.lastName}</td>
+              <td>{person.zip}</td>
+              <td>{person.email}</td>
+              <td>{person.city}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
